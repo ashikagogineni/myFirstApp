db3ed92b3cc350567685877571f912a93cc57a74
Ashika G <ashi.gogineni@gmail.com>
2023-07-12 14:33:41 -0700

    Initial Commit

---
+++ myFirstApp/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  myFirstApp
+//
+//  Created by scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+        VStack {
+            Text("Here is a picture of the sky!")
+                .font(.title)
+            Image("skyImage")
+                .resizable(resizingMode: .stretch)
+        }
+        .padding(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
